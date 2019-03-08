7a3619b2b170383b34761930c38928430c35f4ee
Tengelin <mtengelin@ea.com>
2019-03-08 11:26:28 +0100

    Handling both EUR and CHF

--- Revolut_Analysis.R
+++ Revolut_Analysis.R
@@ -1,73 +1,64 @@
+# Parameters
+EUR <- 1.14
+GBP <- 1.28
+folder <- "Statements"
+
 # Libraries
 library(data.table)
 library(stringr)
 library(plotly)
 
-ReadStatements <- function(folder){
-    # Read the Revolut statement files, use file names of format [person].csv
-    Filenames <- list.files(folder)
-    Statements <- list()
-    Persons <- rep(NA, length(Filenames))
-    
-    for (i in 1:length(Filenames)) {
-        Statements[[i]] <- fread(paste(folder,Filenames[i], sep = "/"))
-        Persons[i] <- str_replace_all(Filenames[i],".csv","")
-        Statements[[i]]$Card <- Persons[i]
-    }
-    
-    # Merge into one
-    Transactions <- do.call(rbind,Statements)
-    
-    # Remove strange characters
-    Transactions$`Paid In (CHF)` <- str_replace_all(Transactions$`Paid In (CHF)`, "[^0-9.]","")
-    Transactions$`Paid Out (CHF)` <- str_replace_all(Transactions$`Paid Out (CHF)`, "[^0-9.]","")
-    Transactions$`Balance (CHF)` <- str_replace_all(Transactions$`Balance (CHF)`, "[^0-9.]","")
-    
-    # Reformat columns
-    Transactions$Date <- as.Date(Transactions$`Completed Date`, format = "%d %B %Y",tz = "GMT")
-    Transactions$PaidOut <- as.numeric(Transactions$`Paid Out (CHF)`) 
-    Transactions$PaidIn <- as.numeric(Transactions$`Paid In (CHF)`) 
-    Transactions$Balance <- as.numeric(Transactions$`Balance (CHF)`) 
-    Transactions$`Completed Date` <- NULL
-    Transactions$`Paid Out (CHF)` <- NULL
-    Transactions$`Paid In (CHF)` <- NULL
-    Transactions$`Balance (CHF)` <- NULL
-    
-    # Add column for month (yyyy-mm)
-    Transactions$Month <- substr(Transactions$Date, 1, 7)
-    
-    return(Transactions)
+# Read the Revolut statement files, use file names of format [person]-[currency].csv
+Filenames <- list.files(folder)
+Statements <- list()
+Persons <- rep(NA, length(Filenames))
+
+for (i in 1:length(Filenames)) {
+  Statements[[i]] <- fread(paste(folder,Filenames[i], sep = "/"))
+  Persons[i] <- str_replace_all(Filenames[i],".csv","")
+  Statements[[i]]$Card <- Persons[i]
+  Statements[[i]]$Currency <- substr(names(Statements[[i]][,3]),11,13)
+  
+  # Remove currency from column names (3,4,7)
+  setnames(Statements[[i]], 3 ,"PaidOut")
+  setnames(Statements[[i]], 4 ,"PaidIn")
+  setnames(Statements[[i]], 7 ,"Balance")
 }
 
-PlotExpenses <- function(Expenses) {
-    # Plot the expenses, either total or per person
-    Persons <- unique(Expenses$Card)
-    p <- plot_ly(Expenses, x = ~Month, y = ~PaidOut, type = 'bar', 
-                 name = ~Category, color = ~Category,
-                 marker = list(line = list(color = 'rgb(8,48,107)',width = 0.5))) %>%
-        layout(yaxis = list(title = 'CHF'), barmode = 'stack', title = Persons)
-    print(p)
-}
+# Merge into one
+Transactions <- do.call(rbind,Statements)
 
-# Read all statements in "Statements" folder
-Transactions <- ReadStatements("Statements")
+# Remove strange characters
+Transactions$`PaidIn` <- str_replace_all(Transactions$`PaidIn`, "[^0-9.]","")
+Transactions$`PaidOut` <- str_replace_all(Transactions$`PaidOut`, "[^0-9.]","")
+Transactions$`Balance` <- str_replace_all(Transactions$`Balance`, "[^0-9.]","")
+
+# Reformat date column, add year if missing (newer version of the app)
+Transactions$Date <- as.Date(Transactions$`Completed Date`, format = "%d %B %Y",tz = "GMT") # Replace day month year
+Transactions[is.na(Transactions$Date),]$Date <- as.Date(Transactions[is.na(Transactions$Date),]$`Completed Date`, format = "%Y %B %d",tz = "GMT") # Replace year month day
+Transactions[is.na(Transactions$Date),]$Date <- as.Date(Transactions[is.na(Transactions$Date),]$`Completed Date`, format = "%B %d",tz = "GMT") # Replace year month day
+
+# Reformat columns
+Transactions$`Completed Date` <- NULL
+Transactions$PaidOut <- as.numeric(Transactions$PaidOut)
+Transactions$PaidIn <- as.numeric(Transactions$PaidIn)
+Transactions$Balance <- as.numeric(Transactions$Balance)
+
+
+# Add column for month (yyyy-mm)
+Transactions$Month <- substr(Transactions$Date, 1, 7)
+
+# Add column for CHF equivalent
+Transactions$PaidOutCHF <- Transactions$PaidOut
+Transactions[Currency=="EUR"]$PaidOutCHF <- Transactions[Currency=="EUR"]$PaidOut*EUR
+Transactions[Currency=="GBP"]$PaidOutCHF <- Transactions[Currency=="GBP"]$PaidOut*GBP
 
 # Save CSV file with all transactions
-write.csv2(Transactions, "All_Transactions.csv", row.names = FALSE)
+fwrite(Transactions, "All_Transactions.csv")
 
 # Create Expenses (Transactions minus transfers)
 Expenses <- Transactions[Category != 'transfers']
 
-# Aggregate expenses by category, month and card for plotting
-Expenses <- Expenses[, sum(PaidOut), .(Category, Month, Card)]
-names(Expenses)[4]<-"PaidOut"
-
-# Plot monthly summary
-PlotExpenses(Expenses)
-
-# Plot monthly summary by card
-Persons <- unique(Expenses$Card)
-for (i in 1:length(Persons)) {
-    PlotExpenses(Expenses[Card == Persons[i]])
-}
+# Save CSV file with all transactions
+fwrite(Expenses, "All_Expenses.csv")
 
